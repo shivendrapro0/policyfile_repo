be332e365672fec736f6ff5963d2427d1449d433
Shivendra <shivendrapro0@gmail.com>
2018-06-28 07:34:18 +0000

    Inital checkin

---
+++ ol-server.rb
@@ -0,0 +1,27 @@
+# Policyfile.rb - Describe how you want Chef to build your system.
+#
+# For more information on the Policyfile feature, visit
+# https://docs.chef.io/policyfile.html
+
+# A name that describes what the system you're building with Chef does.
+name "ol-server"
+
+# Where to find external cookbooks:
+default_source :supermarket
+
+# run_list: chef-client will run these recipes in the order specified.
+run_list "testbook::default"
+
+# Specify a custom source for a single cookbook:
+cookbook "testbook", git: 'https://github.com/shivendrapro0/testbook.git', branch: 'master'
+
+default['qa'] = {
+  testbook: {
+    starter_name: 'Amrita TOmar'
+  }
+}
+default['dev'] = {
+  testbook: {
+    starter_name: 'Shivendra Sharma'
+  }
+}
